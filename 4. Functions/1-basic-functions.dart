bff3444a44f445dc22fbf34c66aad72092c43905
Muhammad Ibtisam Afzal <chaudhryibtisam2003@gmail.com>
2025-03-15 12:19:08 +0500

    updated basic functions

--- 4. Functions/1-basic-functions.dart
+++ 4. Functions/1-basic-functions.dart
@@ -1,4 +1,5 @@
 
+// Functions are a set of statements that perform a task or calculate a value.
 void main() {
     sayHello();
     int area = calculateArea(10, 5);
@@ -6,11 +7,12 @@ void main() {
 
 }
 
+// Function without return type
 void sayHello(){
     print("Hello");
 }
 
+// Function with return type
 int calculateArea(int a, int b){
     return a * b; 
-       
 }
